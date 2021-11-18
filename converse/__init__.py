a503095de1fc133a041aeed5f39beac532f62116
AnjanaRita <ritaanjana1993@gmail.com>
2021-11-18 16:58:26 +0530

    init  boilerplate and setup

---
+++ converse/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.0.1'
+from .insights import Callyzer
+from .speaker_stats import SpeakerStats
+from .nlp_insights import TopicFinder
+from .segmentation import SemanticTextSegmention
